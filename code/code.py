fd3232d1fda48600fc0d46dacd8cdaafb0d9e245
Valentin Haenel <valentin.haenel@gmx.de>
2013-04-09 17:44:44 +0200

    better Python example

--- code/code.py
+++ code/code.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
 
+
 class HelloWorld(object):
+    """ Hello World in Python using classes. """
 
     def __init__(self):
         self.message = 'Hello World!'
-
 
     def __str__(self):
         return self.message
